c0972dc8b234d65eed1a907abc725f091311f054
retsukodjo@gmail.com <retsukodjo@gmail.com>
2023-12-12 14:00:40 +0000

    Basic home screen, location screen and edit location button.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
-class home extends StatefulWidget {
-  const home({super.key});
+
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<home> createState() => _homeState();
+  State<Home> createState() => _HomeState();
 }
 
-class _homeState extends State<home> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home Screen')),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+                // Add your button press logic here
+              },
+              icon: Icon(Icons.edit_location), // Placeholder icon
+              label: Text('Edit Location'), // Placeholder label
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
